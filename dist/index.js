2924a9181410abbc1412e22c8a6adbbd6c0cd73e
dilshandez <dilshandez6@gmail.com>
2023-12-28 15:30:42 +0530

    Completed index.tx v1

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const products_1 = __importDefault(require("./products"));
 // Store the name of the product the user is trying to find.
 const productName = 'shirt';
-// Find product by its name from our list of products
+const shippingAddress = '4 Privet Drive, Little Whinging, Surrey';
+let shipping = 10;
+let taxPercent = 4;
+const taxTotal = 5;
+const total = 35;
+// Free shipping
+if (total > 25) {
+    shipping = 0;
+    console.log("You'll get FREE shipping!\n");
+}
+// NY Tax
+if (shippingAddress.match('New York')) {
+    taxPercent += 10;
+}
 const product = products_1.default.filter(product => {
     if (product.name !== productName) {
         return false;
@@ -15,3 +28,16 @@ const product = products_1.default.filter(product => {
 });
 // Verify we found the correct product
 console.log(product);
+// Notify preorder shipping status
+if (product[0].preOrder == 'true') {
+    console.log("We'll notify you when the product is being shipped.");
+}
+// Print the bill
+console.log(`
+    Product name: ${productName}
+    Shipping address: ${shippingAddress}
+    Price of the product: ${product.length > 0 ? product[0].price : 'N/A'}
+    Tax total: ${taxTotal}
+    Shipping: ${shipping}
+    Total amount: ${total}
+`);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,30 @@ import products from "./products";
 
 // Store the name of the product the user is trying to find.
 const productName: string = 'shirt';
+const shippingAddress: string = '4 Privet Drive, Little Whinging, Surrey';
+let shipping: number = 10;
+let taxPercent: number = 4;
+const taxTotal: number = 5;
+const total: number = 35;
+
+// Free shipping
+if(total>25) {
+    shipping = 0;
+    console.log("You'll get FREE shipping!\n")
+}
+
+// NY Tax
+if (shippingAddress.match('New York')) {
+    taxPercent += 10;
+}
 
 // Find product by its name from our list of products
+interface product {
+  name: string;
+  price: string;
+  preOrder: string;
+}
+
 const product = products.filter(product=>{
     if(product.name !== productName) {
         return false;
@@ -13,3 +35,18 @@ const product = products.filter(product=>{
 
 // Verify we found the correct product
 console.log(product);
+
+// Notify preorder shipping status
+if(product[0].preOrder == 'true') {
+    console.log("We'll notify you when the product is being shipped.");
+}
+
+// Print the bill
+console.log(`
+    Product name: ${productName}
+    Shipping address: ${shippingAddress}
+    Price of the product: ${product.length > 0 ? product[0].price : 'N/A'}
+    Tax total: ${taxTotal}
+    Shipping: ${shipping}
+    Total amount: ${total}
+`);
